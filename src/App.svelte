199f6daf64aef3fffdeb39f5da1ecf9436e0c7b3
Jascha030 <contact@jaschavanaalst.nl>
2021-08-29 05:51:35 +0200

    App component

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Palette from "./lib/Palette.svelte";
+  import path from 'path';
+
+  let theme = "theme.json";
+</script>
+
+<main>
+  <Palette themeJSON="{theme}" />
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      margin:0;
+      padding:0;
+  }
+
+  main {
+    margin: 0 auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
